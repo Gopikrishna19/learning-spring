824a97698d0666f3a7767e6fcadf20aa000e2417
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-06-16 14:51:44 -0500

    alternatively use constructor injector

--- src/main/java/com/gopikrishna19/learningSpring/service/CustomerServiceImpl.java
+++ src/main/java/com/gopikrishna19/learningSpring/service/CustomerServiceImpl.java
@@ -2,18 +2,19 @@ package com.gopikrishna19.learningSpring.service;
 
 import com.gopikrishna19.learningSpring.model.Customer;
 import com.gopikrishna19.learningSpring.repositories.CustomerRepository;
+import com.gopikrishna19.learningSpring.repositories.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
     private CustomerRepository customerRepository;
 
+    public CustomerServiceImpl(HibernateCustomerRepositoryImpl repository) {
+        customerRepository = repository;
+    }
+
     @Override
     public List<Customer> findAll() {
         return customerRepository.findAll();
-    }
-
-    public void setCustomerRepository(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
     }
 }
