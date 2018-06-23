33f69702b7903bac994a4ab65df2277b42a855dc
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-06-23 12:25:52 -0500

    add messages to drive content of the page dynamically

--- FitnessTracker/src/main/java/ApplicationConfig.java
+++ FitnessTracker/src/main/java/ApplicationConfig.java
@@ -1,6 +1,8 @@
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,6 +18,15 @@ public class ApplicationConfig implements WebMvcConfigurer {
         resolver.setPrefix("/views/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    @Bean(name = "messageSource")
+    public static MessageSource getMessageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        source.setUseCodeAsDefaultMessage(true);
+        source.setDefaultEncoding("UTF-8");
+        return source;
     }
 
     @Override
--- FitnessTracker/src/main/webapp/views/add-minutes.jsp
+++ FitnessTracker/src/main/webapp/views/add-minutes.jsp
@@ -1,10 +1,11 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <h1>Add minutes exercised</h1>
 <form:form modelAttribute="exercise">
     <table>
         <tr>
-            <td>Minutes exercised for today:</td>
+            <td><spring:message code="goal.text"/></td>
             <td><form:input path="minutes"/></td>
         </tr>
         <tr>
